95f19ef0ede0892262b07bdfa0fd04a0682a550f
Anthony K. @Big Mac <off.koss@gmail.com>
2014-11-06 12:31:31 +0300

    Change dates in footer to roman numbers

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -247,7 +247,7 @@
     <div class="row">
       <div class="large-12 columns">
         <div class="copyright">
-          <a href="http://creativecommons.org/licenses/by/3.0/deed.ru" rel="license"><img src="http://i.creativecommons.org/l/by/3.0/80x15.png" style="border-width:0" alt="Лицензия Creative Commons"></a> 2007 &ndash; <?php print date('Y') ?> Kossoff.ru <?php print '(' . check_plain($site_name) . ') ' . t('Some rights reserved.'); ?>
+          <a href="http://creativecommons.org/licenses/by/3.0/deed.ru" rel="license"><img src="http://i.creativecommons.org/l/by/3.0/80x15.png" style="border-width:0" alt="Лицензия Creative Commons"></a> MMVII &ndash; <?php print number_to_roman (date('Y')); ?> Kossoff.ru <?php print '(' . check_plain($site_name) . ') ' . t('Some rights reserved.'); ?>
         </div>
       </div>
     </div>
